804f352ffd25f8ddbf2d2d94443a10c925ff1b5f
Ycaro02 <ycaro02@hotmailfr>
2024-02-13 19:49:40 +0100

    save continue test

--- src/test.s
+++ src/test.s
@@ -2,5 +2,8 @@ section .text
 	global ft_strlen	; set entry point
 
 ft_strlen:			; int strlen(char *s);
-	mov rax, 0		; put 0 in rax (contained the return  value)
+	push	rbp		; Save the stack frame of caller, pushed on stack 
+	mov 	rbp, rsp	; Set my own stack frame
+	pop 	rbp			; Pop rbp to restore stack
+	mov 	rax, 42		; put 0 in rax (contained the return  value)
 	ret
--- tester/src/test.c
+++ tester/src/test.c
@@ -1,9 +1,19 @@
 # include "../../libasm.h"
 # include <stdio.h>
 
+int basic_strlen(char *str)
+{
+    int i = 0;
+    while (str[i]) {
+        i++;
+    }
+    return (i);
+}
+
 int main(void)
 {
     char *str = "koala";
-    printf("len de %s %d\n", str, ft_strlen(str));
+    printf("str:%s\nAssembly len |%d|\n", str, ft_strlen(str));
+    printf("Basic strlen |%d|\n", basic_strlen(str));
     return (0);
 }
