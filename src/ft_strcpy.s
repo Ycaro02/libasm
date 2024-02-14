ebdd1309b1ae4d05a5e527830e7949547f035059
Ycaro02 <ycaro02@hotmailfr>
2024-02-14 11:04:52 +0100

    refact tsrcpy test andd add protection against to long string for src

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -11,32 +11,41 @@ ft_strcpy:			; int strlen(char *s);
 	push	rdi				; push rdi on stack 1st args
 	push	rsi				; push rci on stack 2second args
 	push	rcx				; push rcx on stack counter
+	push	rdx				; second count
 
 _strcpy_protect:
 	cmp		rdi, 0x0		; rdi NULL
 	je		_strcpy_exit
 	cmp		rsi, 0x0		; rsi NULL	
 	je		_strcpy_exit
-; need to check for mem surperposition
-; need to check dest size
+	call 	ft_strlen		; compute dest len
+	mov		rdx, rax		; store len in rdx
+; need to check for string memory surperposition
+
 _strcpy_init:
 	xchg	rdi, rsi		; exchange data between rsi and rdi
 	call	ft_strlen		; call ft_strlen on rdi --> src, store result in rax
 	xchg	rdi, rsi		; restore initiale rsi rdi data
 	mov		rcx, rax		; store src_size in rcx
+	
+	cmp		rdx, rcx		; compare dest_len, src_len
+	jl		_strcpy_exit	; jump lower exit
 
 _strcopy_loop:
 	cld						; clear direction flag
 	xor		al, al			; set al to 0
 	repz movsb				; repeat until *rdi != al -> '\0'
+	mov		BYTE [rdi], 0x0	; need to put '\0'
 
 _strcpy_exit:
-	mov		BYTE [rdi], 0x0; need to put '\0'
+	pop		rdx
 	pop		rcx
 	pop		rsi
 	pop		rdi
 	mov		rax, rdi		; return src pointer
 	ret
+
+
 
 ; Les deux chaînes ne doivent pas se chevaucher. 
 ; La chaîne dest doit être assez grande pour accueillir la copie.
--- tester/src/test.c
+++ tester/src/test.c
@@ -2,10 +2,11 @@
 # include "../basic_define.h"
 # include <stdio.h>
 # include <stdlib.h>
+#include <string.h>
 
-int basic_strlen(char *str)
+size_t basic_strlen(char *str)
 {
-    int i = 0;
+    size_t i = 0;
     while (str[i]) {
         i++;
     }
@@ -15,25 +16,62 @@ int basic_strlen(char *str)
 static int test_strlen()
 {
     char *str = "koala";
-    printf("str:%s\nAssembly len |%d|\n", str, ft_strlen(str));
-    printf("Basic strlen |%d|\n", basic_strlen(str));
+    printf("str:%s\nAssembly len |%lu|\n", str, ft_strlen(str));
+    printf("Basic strlen |%lu|\n", basic_strlen(str));
     str = NULL;
-    printf("str:NULL\nAssembly len |%d|\n", ft_strlen(str));
+    printf("str:NULL\nAssembly len |%lu|\n", ft_strlen(str));
     str = "heylolsdasdadsa";
-    printf("str:%s\nAssembly len |%d|\n", str, ft_strlen(str));
+    printf("str:%s\nAssembly len |%lu|\n", str, ft_strlen(str));
     str = "very longnnfjf lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng";
-    printf("str:%s\nAssembly len |%d|\n", str, ft_strlen(str));
+    printf("str:%s\nAssembly len |%lu|\n", str, ft_strlen(str));
     str = "";
-    printf("str:%s\nAssembly len |%d|\n", str, ft_strlen(str));
+    printf("str:%s\nAssembly len |%lu|\n", str, ft_strlen(str));
     return (0);
 
 }
 
 
+static int strcpy_null_test()
+{
+    int max = 10, ret = 0;
+    char *tmp = malloc(sizeof(char) * max);
+    for (int i = 0; i < max; ++i) {
+        tmp[i] = 'Y';
+    }
+    tmp[max - 1] = '\0';
+    char *save = strdup(tmp);
+    
+    printf("Test ft_strcpy(NULL, NULL)\n");
+    ft_strcpy(NULL, NULL);
+    if (strcmp(save, tmp) != 0) {
+        ret = 1;
+    } 
+    else 
+    {
+        printf("Test ft_strcpy(tmp, NULL)\n");
+        tmp = ft_strcpy(tmp, NULL);
+        printf("tmp = %s\n", tmp);
+        if (strcmp(save, tmp) != 0) {
+            ret = 1;
+        } 
+        else {
+            printf("Test ft_strcpy(NULL, tmp)\n");
+            ft_strcpy(NULL, tmp);
+            if (strcmp(save, tmp) != 0) {
+                ret = 1;
+            }
+        }
+    }
+    free(save);
+    free(tmp);
+    return (ret);
+    
+}
+
 static int test_strcpy()
 {
     char *str = "alo";
-    int max = 10;
+    int max = 10, ret = 0;
     char *tmp = malloc(sizeof(char) * max);
     for (int i = 0; i < max; ++i) {
         tmp[i] = 'Y';
@@ -43,11 +81,17 @@ static int test_strcpy()
     tmp = ft_strcpy(tmp, str);
     printf("ret strcpy = %s\n", tmp);
     
-    char *big = "1234567890123456789";
+    char *save = strdup(tmp);
+    
+    char *big = "1234567";
     tmp = ft_strcpy(tmp, big);
-    printf("ret strcpy = %s\n", tmp);
+    if (strcmp(save, tmp) != 0) {
+        ret = 1;
+    }
     free(tmp);
-    return (0);
+    free(save);
+    ret = strcpy_null_test();
+    return (ret);
 
 }
 
