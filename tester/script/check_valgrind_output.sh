2556871f446ef136031c88a18fd1ab28d8ee6457
Ycaro02 <ycaro02@hotmailfr>
2024-02-17 12:29:02 +0100

    make av check for leak too

--- tester/script/check_valgrind_output.sh
+++ tester/script/check_valgrind_output.sh
@@ -6,16 +6,27 @@ CYAN="\e[36m"
 
 VAL_OUT=valgrind_output
 
+CHECK_LEAK="All heap blocks were freed -- no leaks are possible"
+
+# maybe we can mute else and just display error
+
 cat ${VAL_OUT} | grep "Invalid" > /dev/null 2> /dev/null;
 if [ $? -eq 0 ]; then
-	echo -e "${RED}Invalid read/write found $1 $RESET"
+	echo -e "${RED}Invalid read/write found in $1 $RESET"
 else
 	echo -e "${CYAN}No invalid read/write found in $1 $RESET"
 fi
 
 cat ${VAL_OUT} | grep "Conditional" > /dev/null 2> /dev/null;
 if [ $? -eq 0 ]; then
-	echo -e "${RED}Conditional jump found $1 $RESET"
+	echo -e "${RED}Conditional jump found in $1 $RESET"
 else
 	echo -e "${CYAN}No Conditional jump found in $1 $RESET"
+fi
+
+cat ${VAL_OUT} | grep ${CHECK_LEAK} > /dev/null 2> /dev/null;
+if [ $? -eq 0 ]; then
+	echo -e "${RED}Leak found in $1 $RESET"
+else
+	echo -e "${CYAN}No leak found in $1 $RESET"
 fi
