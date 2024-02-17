e2f70051d6757d4e014fe10ccf0825f417f42b7f
Ycaro02 <ycaro@Kali.home>
2024-02-17 14:36:32 -0600

    list sort is working

--- src/bonus/ft_list_sort.s
+++ src/bonus/ft_list_sort.s
@@ -16,32 +16,32 @@ ft_list_sort:
 	sub			rsp, 0x30				; sub 0x30 (48 byte)
 	mov			qword[rbp-0x28], rdi	; **begin_lst in 0x28
 	mov			qword[rbp-0x30], rsi	; *cmp in 0x30
-	
 	cmp			qword[rbp-0x30], 0x0	; compare cmp and null
 	je			_exit_label
-	
 	cmp			qword[rbp-0x28], 0x0	; compare **begin_lst and null
 	je			_exit_label
-	
 	mov			rax, qword[rbp-0x28]	; **begin_lst in rax
 	mov			rax, qword[rax]			; dereference it : *begin_lst
 	test		rax, rax				; test if null
 	je			_exit_label
 	
 	mov			qword[rbp-0x10], rax	; put rax (*begin_lst) in 0x10 (current)
-	mov			qword[rbp-0x18], rax	; init current with same value
+	mov			qword[rbp-0x18], rax	; init next with same value
 	jmp			_list_sort_loop_check
 
   _list_sort_loop:
 	mov			rax, qword[rbp-0x10]	; put current in rax
-	mov			rax, qword[rax+0x8]		; rax = current->next
+	; mov			rax, qword[rax+0x8]		; rax = current->next
+	mov			rax, qword[rax]		; rax = current->next
 	mov			qword[rbp-0x18], rax	; store this in next ---> next = current->next
 	
 	mov			rax, qword[rbp-0x10]	; put current in rax
-	mov			rdx, qword[rax]			; get current->data in rdx
+	; mov			rdx, qword[rax]			; get current->data in rdx
+	mov			rdx, qword[rax+0x8]			; get current->data in rdx
 	
 	mov			rax, qword[rbp-0x18]	; put next in rax
-	mov			rax, qword[rax]			; get next->data in rax
+	; mov			rax, qword[rax]			; get next->data in rax
+	mov			rax, qword[rax+0x8]			; get next->data in rax
 
 	mov			rcx, qword[rbp-0x30]	; put *cmp, function ptr in rcx
 	; setup args for cmp call
@@ -53,18 +53,22 @@ ft_list_sort:
 	jg			_list_sort_inc_next		; jump if greater 
 	; if lower or equal
 	mov			rax, qword[rbp-0x10]	; put current in rax
-	mov			rax, qword[rax]			; get current data
+	; mov			rax, qword[rax]			; get current data
+	mov			rax, qword[rax+0x8]			; get current data
 	mov			qword[rbp-0x8], rax		; save this data in tmp --> tmp_data = current->data
 	
 	mov			rax, qword[rbp-0x18]	; put next in rax
-	mov			rdx, qword[rax]			; get next data in rdx
+	; mov			rdx, qword[rax]			; get next data in rdx
+	mov			rdx, qword[rax+0x8]			; get next data in rdx
 	
 	mov			rax, qword[rbp-0x10]	; put current in rax
-	mov			qword[rax], rdx			; current->data = next->data
+	; mov			qword[rax], rdx			; current->data = next->data
+	mov			qword[rax+0x8], rdx			; current->data = next->data
 	
 	mov			rax, qword[rbp-0x18]	; put next in rax
 	mov			rdx, qword[rbp-0x8]		; put save data (tmp) in rdx
-	mov			qword[rax], rdx			; next->data = tmp
+	; mov			qword[rax], rdx			; next->data = tmp
+	mov			qword[rax+0x8], rdx			; next->data = tmp
 	mov			rax, qword[rbp-0x28]	; put **lst in rax
 	mov			rax, qword[rax]			; rax = *lst
 	mov			qword[rbp-0x10], rax	; current = *lst
@@ -80,7 +84,8 @@ ft_list_sort:
 	cmp			qword[rbp-0x10], 0x0	; if current == NULL
 	je			_exit_label
 	mov			rax, qword[rbp-0x10]	; store current in rax
-	mov			rax, qword[rax+0x8]		; increment to next field (current->next)
+	; mov			rax, qword[rax+0x8]		; increment to next field (current->next)
+	mov			rax, qword[rax]		; increment to next field (current->next)
 	test		rax, rax				; test if current->next == NULL
 	jne			_list_sort_loop			; if not null jump to loop
 
