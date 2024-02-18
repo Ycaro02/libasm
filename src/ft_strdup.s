1fd6640a38a7567799a2198f9d52f2a5cc8c1161
Ycaro02 <ycaro@Kali.home>
2024-02-18 10:19:03 -0600

    review clean strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -10,41 +10,39 @@ extern  malloc
 ; rdi --> char *src --> src string to copy
 
 ft_strdup:
-    push		rdi             ; save rdi on stack (str ptr)
-    cmp			rdi, 0x0        ; cmp rdi NULL
-    je			_strdup_error ; if NULL jmp
+    push		rdi             	; save rdi on stack (str ptr)
+    cmp			rdi, 0x0        	; cmp rdi NULL
+    je			_strdup_error 		; if NULL jmp
     call		ft_strlen
+
 ; now we got len but we need to mov this value in rdi for call malloc
 _strdup_alloc:
-    mov			rdi, rax	    ; get len in rdi
-    add			rdi, 0x1        ; one add byte to RDI for '\0'
-    push		rdi             ; save rdi on stack (len value) cause malloc reset/change lot of register
-
+    mov			rdi, rax	    	; get len in rdi
+    add			rdi, 0x1        	; one add byte to RDI for '\0'
+    push		rdi             	; save rdi on stack (len value) cause malloc reset/change lot of register
     call		malloc  WRT ..plt   ; call malloc with RDI args, len + 1
     pop			rcx                 ; got back str len value
     cmp			rax, 0x0            ; malloc return addr is now in RAX if malloc return NULL
     je			_strdup_error
-    sub			rcx, 0x1    ; avoid invalid write
-
-    mov			rdi, rax    ; put malloc in rdi
-    pop			rsi         ; get back string ptr
-    push		rdi         ; save head of malloc addr
+    sub			rcx, 0x1    		; avoid invalid write
+    mov			rdi, rax    		; put malloc in rdi
+    pop			rsi         		; get back string ptr
+    push		rdi         		; save head of malloc addr
 
 ; RCX: str len
 ; RSI: MALLOC ADDR
 ; RDI: ADDR str to copy
 _strdup_loop:
-	cld							; clear direction flag
-	xor			al, al			; set al to 0
-	repz		movsb			; repeat while zero, until the count register (CX) or the zero flag (ZF) matches a tested condition
-	mov			byte[rdi], 0x0	; need to put '\0' at the end of string
+	cld								; clear direction flag
+	xor			al, al				; set al to 0
+	repz		movsb				; repeat while zero, until the count register (CX) or the zero flag (ZF) matches a tested condition
+	mov			byte[rdi], 0x0		; need to put '\0' at the end of string
 
 _strdup_exit:
-    pop			rax             ; get head of malloc addr
+    pop			rax             	; get head of malloc addr
     ret
 
 _strdup_error:
     pop			rdi
-    mov			rax, 0x0    ; set rax to 1 if rdi is NULL
+    mov			rax, 0x0    		; set rax to 0 beforer error ret
     ret
-    ; jmp _strdup_alloc 
