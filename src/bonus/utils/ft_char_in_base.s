8990bbcc6631c8212ddc0df6240f87fba687b357
Ycaro02 <ycaro02@hotmailfr>
2024-02-21 14:42:43 +0100

    continue check clean

--- src/bonus/utils/ft_char_in_base.s
+++ src/bonus/utils/ft_char_in_base.s
@@ -2,9 +2,11 @@ BITS 64
 
 section .text
 	global ft_char_in_base
+
 ; RDI, EDI : char c 0x4
 ; RSI char* base	0x10
 ; Dump of assembler code for function char_in_base:
+
 ft_char_in_base:
 push	rbp					;save rbp
 mov		rbp, rsp				
@@ -18,19 +20,19 @@ add		qword [rbp-0x10], 0x1	; increment base ptr
 
 _char_in_base_loop:
 mov		rax, qword [rbp-0x10]	; store base ptr in rax
-movzx	eax, byte [rax]		; get byte (first char)
+movzx	eax, byte [rax]			; get byte (first char)
 test	al, al					; test if 0
 je		_char_in_base_end		; jump if equal
 mov		rax, qword [rbp-0x10]	; else get base ptr in rax
-movzx	eax, byte [rax]		; got byte again REALLY mandatory ???
+movzx	eax, byte [rax]			; got byte again REALLY mandatory ???
 cmp		byte [rbp-0x4], al		; cmp char c with given char --> c == *base
-jne		_char_in_base_start	; if not equal jmp start --> continue while if c != *base
+jne		_char_in_base_start		; if not equal jmp start --> continue while if c != *base
 
 _char_in_base_end:
 mov		rax, qword [rbp-0x10]	; move count in rax
-movzx	eax, byte [rax]		; move it in eax, get *base
+movzx	eax, byte [rax]			; move it in eax, get *base
 test	al, al					; if 0
 setne	al						; if no equal: al == 1 else al = 0 --> https://web.itu.edu.tr/kesgin/mul06/intel/instr/setne_setnz.html
-movzx	eax, al				; move al in eax, 32 bits return value (int)
-pop		rbp					; restore rbp
-ret		; return
+movzx	eax, al					; move al in eax, 32 bits return value (int)
+pop		rbp						; restore rbp
+ret
--- src/bonus/utils/ft_create_list_node.s
+++ src/bonus/utils/ft_create_list_node.s
@@ -12,10 +12,10 @@ extern  malloc
 ft_create_list_node:
 	push   rbp						; save rbp
 	mov    rbp, rsp					;
-	sub    rsp, 0x20					; allocate 0x20 on stack
+	sub    rsp, 0x20				; allocate 0x20 on stack
 	mov    qword[rbp-0x18], rdi		; store lst head at 0x18
-	mov    edi, 0x10					; mov sizeof t_list in rdi
-	call   malloc  WRT ..plt			; call malloc
+	mov    edi, 0x10				; mov sizeof t_list in rdi
+	call   malloc  WRT ..plt		; call malloc
 	mov    qword[rbp-0x8], rax		; store malloc return in node var 0x8
 	cmp    qword[rbp-0x8], 0x0		; chec if 0 NULL
 	jne    _list_node_init			; if no equal set value
@@ -24,10 +24,10 @@ ft_create_list_node:
 
 _list_node_init:
 	mov    rax, qword[rbp-0x8]		; put node in rax
-	mov    qword[rax+0x8], 0x0			; node->next = NULL 
+	mov    qword[rax+0x8], 0x0		; node->next = NULL 
 	mov    rax, qword[rbp-0x8]		; put in node in rax
 	mov    rdx, qword[rbp-0x18]		; put lst head pointer/addr in rdx
-	mov    qword[rax], rdx		; node->data = rdx
+	mov    qword[rax], rdx			; node->data = rdx
 	mov    rax, qword[rbp-0x8]		; set turen value = node
 
 _list_node_exit:
