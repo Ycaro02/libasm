04c02f9c0202c7421e59f74cf140c59f0abc8be3
Ycaro02 <ycaro@Kali.home>
2024-02-27 15:02:51 -0600

    finish strlen comment

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -5,19 +5,20 @@ section .text
 
 ft_strlen:							; int strlen(char *s);
 	push			rdi				; push rdi on stack to save it
-	xor				rcx, rcx		; initial value 0, for blank or null string 
-	cmp				rdi, 0x0		; rdi NULL
+	xor				rcx, rcx		; initial rcx value 0, for blank or null string 
+	cmp				rdi, 0x0		; compare rdi NULL
 	je				_ft_strlen_exit ; jump equal exit
 	sub				rcx, 0x1		; rcx -= 1 to enter in while, rcx = 0xfffffffffffff
 
 _ft_strlen_loop:
 	cld								; clear DIRECTION FLAG
 	xor				al, al			; Set al to 0
-	repnz	scasb					; CORE instruction repeat until *rdi != al, decrement rcx, increment rdi each iteration
+	; REPeat No Zero flag, SCAn String Byte
+	repnz	scasb					; CORE instruction repeat until (*rdi != al) ==> (Zero Flag) && rcx != 0; decrement rcx, increment rdi each iteration
 	not				rcx				; reverse rcx
 	sub				rcx, 1			; rcx -= 1
 
 _ft_strlen_exit:
 	mov 			rax, rcx		; put return value in rax
-	pop 			rdi				; pop rdi from stack
+	pop 			rdi				; pop rdi from stack, restore rdi
 	ret
